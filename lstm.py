64f4ece8dc73732f917b7377dbb4b84995aabba4
pammelang <poppyfights@gmail.com>
2018-07-13 16:30:47 +0800

    Updated LSTM model

--- lstm.py
+++ lstm.py
@@ -6,23 +6,41 @@ import torch.optim as optim
 torch.manual_seed(1)
 
 class LSTMquestion(nn.Module):
-    def __init__(self, vocab_size, embedding_dim=300, hidden_dim=1024, max_seq_ln=15):
+    def __init__(self, vocab_size, embedding_dim=300, hidden_dim=1024, drop=0.5):
         super(LSTMquestion, self).__init__()
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
+        self.drop = nn.Dropout(drop)
+        self.tanh = nn.Tanh()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-        self.hidden = (torch.zeroes(1, 1, hidden_dim),
-                        torch.zeroes(1, 1, hidden_dim))
-        self.max_seq_ln = max_seq_ln
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=1)
+    
 
-    def forward(self, question, q_len):
-        embeds = self.embeddings(question)
-        embeds = pack_padded_sequence(embeds, q_len, batch_first=True)
-        lstm_out, self.hidden = self.lstm(
-            embeds.view(len(question), 1, -1), self.hidden)
-        )
-        return lstm_out
+        self._init_lstm(self.lstm.weight_ih_l0)
+        self._init_lstm(self.lstm.weight_hh_l0)
+        self.lstm.bias_ih_l0.data.zero_()
+        self.lstm.bias_hh_l0.data.zero_()
+
+        init.xavier_uniform(self.embedding.weight)
+
+    def _init_lstm(self, weight):
+        for w in weight.chunk(4, 0):
+            init.xavier_uniform(w)
+
+    def forward(self, q, q_len):
+        embedded = self.embedding(q)
+        tanhed = self.tanh(self.drop(embedded))
+        packed = pack_padded_sequence(tanhed, q_len, batch_first=True)
+        _, (_, c) = self.lstm(packed)
+        return c.squeeze(0)
+
+    # def forward(self, question, q_len):
+    #     embeds = self.embeddings(question)
+    #     embeds = pack_padded_sequence(embeds, q_len, batch_first=True)
+    #     lstm_out, self.hidden = self.lstm(
+    #         embeds.view(len(question), 1, -1), self.hidden)
+    #     )
+    #     return lstm_out
         # classes = do linear mapping(lstm_out.view(len(word), -1))
         # scores = do F.log_softmax(classes, dim=1)
                         
